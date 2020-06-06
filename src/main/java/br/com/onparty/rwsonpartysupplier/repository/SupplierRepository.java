ceb2eb04990e500c8d24c656bdd92d633a6f4b08
eduardosukeda <eduardosukeda@live.com>
2020-06-06 09:48:00 -0300

    first commit

---
+++ src/main/java/br/com/onparty/rwsonpartysupplier/repository/SupplierRepository.java
@@ -0,0 +1,9 @@
+package br.com.onparty.rwsonpartysupplier.repository;
+
+import br.com.onparty.rwsonpartysupplier.entity.Supplier;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SupplierRepository extends JpaRepository<Supplier, Integer> {
+}
